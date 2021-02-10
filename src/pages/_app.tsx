24119fc10e4100f7f2bc8196d1867f1d26f98096
rasreee <lesleychang@berkeley.edu>
2021-02-10 16:41:11 -0500

    build fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import { AppProps } from "next/app";
 import React from "react";
 import { ThemeProvider } from "styled-components";
