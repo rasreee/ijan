f47775bf4fe52bddaf9ac8811ec02999e5aa293a
rasreee <lesleychang@berkeley.edu>
2021-02-10 14:08:02 -0500

    fix imports & _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,41 +1,25 @@
-import Document, { DocumentContext } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
 import Head from "next/head";
 import React from "react";
-import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            <Head>
-              <meta charSet="utf-8" />
-              <link rel="icon" href="/favicon.ico" />
-              <link rel="shortcut icon" href="/favicon.ico" />
-              <link
-                href="https://fonts.googleapis.com/css?family=Recursive|Poppins|Roboto+Mono"
-                rel="stylesheet"
-              />
-            </Head>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Recursive|Poppins|Roboto+Mono"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
